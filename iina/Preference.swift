5ab255c1e98cf0edc3aa7575968c9fe116aebc02
Hechen Li <lhc199652@gmail.com>
2025-06-16 23:58:09 -0400

    new pref: show sections in sidebar, add help buttons, localize titles

--- iina/Preference.swift
+++ iina/Preference.swift
@@ -459,7 +459,7 @@ struct Preference {
     }
   }
 
-  enum IINAAutoLoadAction: Int, InitializingFromKey {
+  enum IINAAutoLoadAction: Int, InitializingFromKey, CaseIterable {
     case disabled = 0
     case mpvFuzzy
     case iina
