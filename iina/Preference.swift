2880b7f17398404882bf3986be39f70aed2786bb
Hechen Li <lhc199652@gmail.com>
2025-06-16 22:37:47 -0400

    new pref: add video & audio page

--- iina/Preference.swift
+++ iina/Preference.swift
@@ -616,7 +616,7 @@ struct Preference {
     }
   }
 
-  enum HardwareDecoderOption: Int, InitializingFromKey {
+  enum HardwareDecoderOption: Int, InitializingFromKey, CaseIterable {
     case disabled = 0
     case auto
     case autoCopy
@@ -640,7 +640,7 @@ struct Preference {
     }
   }
 
-  enum ToneMappingAlgorithmOption: Int, InitializingFromKey {
+  enum ToneMappingAlgorithmOption: Int, InitializingFromKey, CaseIterable {
     case auto = 0
     case clip
     case mobius
@@ -768,7 +768,7 @@ struct Preference {
 
   }
 
-  enum ReplayGainOption: Int, InitializingFromKey {
+  enum ReplayGainOption: Int, InitializingFromKey, CaseIterable {
     case no = 0
     case track
     case album
